2eec15c62266980fc401a71324573be7d9b97663
kakedashi3 <riki_funk_x@yahoo.co.jp>
2023-04-27 13:05:41 +0900

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -38,7 +38,7 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 0.6,
+      temperature: 2,
       max_tokens: 170,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
