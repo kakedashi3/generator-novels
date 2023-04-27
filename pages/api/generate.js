1a21af6277375e6982762be9799d6625f29a5a02
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2023-04-27 23:34:21 +0900

    温度を変更

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -39,7 +39,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
-      max_tokens: 160,
+      max_tokens: 150,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
