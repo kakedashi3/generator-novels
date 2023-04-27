4c0b5330f616035a51d1465c72f061e101212c8a
kakedashi3 <riki_funk_x@yahoo.co.jp>
2023-04-27 16:18:06 +0900

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -60,5 +60,5 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Write a short story about a ${capitalizedAnimal} who becomes a hero in a fantastical world.`;
+    return `Write a short story about a ${capitalizedAnimal} who becomes a hero in a fantastical world.`;
 }
